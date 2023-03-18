8f476349ff454f75fed5a031059fcc2330fade55
Sebastian Jamrożek <sebastian.jamrozek@gmail.com>
2023-03-18 12:24:30 +0100

    started migration to lua

---
+++ lua/mojzi/init.lua
@@ -0,0 +1,4 @@
+require("mojzi.set")
+require("mojzi.remap")
+require("mojzi.plugins")
+require("mojzi.plugin_config")
---
+++ lua/mojzi/plugin_config/init.lua
@@ -0,0 +1,3 @@
+require("mojzi.plugin_config.nvim-tree")
+require("mojzi.plugin_config.nvim-treesitter")
+require("mojzi.plugin_config.telescope")
